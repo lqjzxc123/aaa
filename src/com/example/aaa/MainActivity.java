9815c0775b1c79fabdb9fcdfb364d6a59d47a673
lqjzxc123 <493663519@qq.com>
2016-12-05 09:42:56 +0800

    aaa
    aaaaaaaaaaaaa

--- src/com/example/aaa/MainActivity.java
+++ src/com/example/aaa/MainActivity.java
@@ -5,11 +5,12 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hdskjfksdj
     }
 
 
